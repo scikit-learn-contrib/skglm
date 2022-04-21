af666a57156f27bc1d48d49a56b40d5e297d80f4
mathurinm <mathurin.massias@gmail.com>
2022-04-21 08:57:51 +0200

    test with large data

--- skglm/gram_solver.py
+++ skglm/gram_solver.py
@@ -120,51 +120,52 @@ def group_lasso(X, y, alpha, groups, max_iter, tol, check_freq=50):
     return w
 
 
-n_samples, n_features = 1_000_000, 300
-X, y, w_star = make_correlated_data(
-    n_samples=n_samples, n_features=n_features, random_state=0)
-alpha_max = norm(X.T @ y, ord=np.inf)
+if __name__ == "__main__":
+    n_samples, n_features = 1_000_000, 300
+    X, y, w_star = make_correlated_data(
+        n_samples=n_samples, n_features=n_features, random_state=0)
+    alpha_max = norm(X.T @ y, ord=np.inf)
 
-# Hyperparameters
-max_iter = 1000
-tol = 1e-8
-reg = 0.1
-group_size = 3
+    # Hyperparameters
+    max_iter = 1000
+    tol = 1e-8
+    reg = 0.1
+    group_size = 3
 
-alpha = alpha_max * reg / n_samples
+    alpha = alpha_max * reg / n_samples
 
-# Lasso
-print("#" * 15)
-print("Lasso")
-print("#" * 15)
-start = time()
-w = lasso(X, y, alpha, max_iter, tol)
-gram_lasso_time = time() - start
-clf_sk = Lasso(alpha, tol=tol, fit_intercept=False)
-start = time()
-clf_sk.fit(X, y)
-celer_lasso_time = time() - start
-np.testing.assert_allclose(w, clf_sk.coef_, rtol=1e-5)
+    # Lasso
+    print("#" * 15)
+    print("Lasso")
+    print("#" * 15)
+    start = time()
+    w = lasso(X, y, alpha, max_iter, tol)
+    gram_lasso_time = time() - start
+    clf_sk = Lasso(alpha, tol=tol, fit_intercept=False)
+    start = time()
+    clf_sk.fit(X, y)
+    celer_lasso_time = time() - start
+    np.testing.assert_allclose(w, clf_sk.coef_, rtol=1e-5)
 
-print("\n")
-print("Celer: %.2f" % celer_lasso_time)
-print("Gram: %.2f" % gram_lasso_time)
-print("\n")
+    print("\n")
+    print("Celer: %.2f" % celer_lasso_time)
+    print("Gram: %.2f" % gram_lasso_time)
+    print("\n")
 
-# Group Lasso
-print("#" * 15)
-print("Group Lasso")
-print("#" * 15)
-start = time()
-w = group_lasso(X, y, alpha, group_size, max_iter, tol)
-gram_group_lasso_time = time() - start
-clf_celer = GroupLasso(group_size, alpha, tol=tol, fit_intercept=False)
-start = time()
-clf_celer.fit(X, y)
-celer_group_lasso_time = time() - start
-np.testing.assert_allclose(w, clf_celer.coef_, rtol=1e-1)
+    # Group Lasso
+    print("#" * 15)
+    print("Group Lasso")
+    print("#" * 15)
+    start = time()
+    w = group_lasso(X, y, alpha, group_size, max_iter, tol)
+    gram_group_lasso_time = time() - start
+    clf_celer = GroupLasso(group_size, alpha, tol=tol, fit_intercept=False)
+    start = time()
+    clf_celer.fit(X, y)
+    celer_group_lasso_time = time() - start
+    np.testing.assert_allclose(w, clf_celer.coef_, rtol=1e-1)
 
-print("\n")
-print("Celer: %.2f" % celer_group_lasso_time)
-print("Gram: %.2f" % gram_group_lasso_time)
-print("\n")
+    print("\n")
+    print("Celer: %.2f" % celer_group_lasso_time)
+    print("Gram: %.2f" % gram_group_lasso_time)
+    print("\n")

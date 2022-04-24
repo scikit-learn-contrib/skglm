d88df2aa1ded4207d07004ae7ce24998579dd195
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2022-04-24 23:43:48 +0200

    added weights

--- skglm/fista_gram.py
+++ skglm/fista_gram.py
@@ -7,9 +7,9 @@ from skglm.utils import ST_vec
 
 
 @njit
-def primal(alpha, r, w):
+def primal(alpha, r, w, weights):
     p_obj = (r @ r) / (2 * len(r))
-    return p_obj + alpha * np.sum(np.abs(w))
+    return p_obj + alpha * np.sum(np.abs(w * weights))
 
 @njit
 def dual(alpha, norm_y2, theta, y):
@@ -19,35 +19,37 @@ def dual(alpha, norm_y2, theta, y):
     return d_obj
 
 @njit
-def dnorm_l1(theta, X):
+def dnorm_l1(theta, X, weights):
     n_features = X.shape[1]
     scal = 0.
     for j in range(n_features):
         Xj_theta = X[:, j] @ theta
-        scal = max(scal, Xj_theta)
+        scal = max(scal, Xj_theta / weights[j])
     return scal
 
 @njit
-def create_dual_point(r, alpha, X):
+def create_dual_point(r, alpha, X, weights):
     theta = r / (alpha * len(y))
-    scal = dnorm_l1(theta, X)
+    scal = dnorm_l1(theta, X, weights)
     if scal > 1.:
         theta /= scal
     return theta
 
 @njit
-def dual_gap(alpha, norm_y2, y, X, w):
+def dual_gap(alpha, norm_y2, y, X, w, weights):
     r = y - X @ w
-    p_obj = primal(alpha, r, w)
-    theta = create_dual_point(r, alpha, X)
+    p_obj = primal(alpha, r, w, weights)
+    theta = create_dual_point(r, alpha, X, weights)
     d_obj = dual(alpha, norm_y2, theta, y)
     return p_obj, d_obj, p_obj - d_obj
 
 
-n_samples, n_features = 1_000_000, 300
+n_samples, n_features = 1_000, 300
 X, y, w_star = make_correlated_data(
     n_samples=n_samples, n_features=n_features, random_state=0)
 alpha_max = norm(X.T @ y, ord=np.inf)
+
+weights = np.random.normal(2, 0.4, n_features)
 
 # Hyperparameters
 max_iter = 1000
@@ -75,17 +77,17 @@ for n_iter in range(max_iter):
     t_new = (1 + np.sqrt(1 + 4 * t_old ** 2)) / 2
     w_old = w.copy()
     z -= (G @ z - Xty) / L / n_samples
-    w = ST_vec(z, alpha / L)
+    w = ST_vec(z, alpha / L * weights)
     z = w + (t_old - 1.) / t_new * (w - w_old)
 
     if n_iter % check_gap_freq == 0:
-        p_obj, d_obj, gap = dual_gap(alpha, norm_y2, y, X, w)
+        p_obj, d_obj, gap = dual_gap(alpha, norm_y2, y, X, w, weights)
         print(f"iter {n_iter} :: p_obj {p_obj:.5f} :: d_obj {d_obj:.5f} " +
               f":: gap {gap:.5f}")
         if gap < tol:
             print("Convergence reached!")
             break
 
-clf = Lasso(alpha, tol=tol, fit_intercept=False)
+clf = Lasso(alpha, tol=tol, weights=weights, fit_intercept=False)
 clf.fit(X, y)
 np.testing.assert_allclose(w, clf.coef_, rtol=1e-5)

02bec1839160e5e0dd5f9d652eb798f4cb386216
mathurinm <mathurin.massias@gmail.com>
2022-04-25 14:45:14 +0200

    update setup information

--- setup.py
+++ setup.py
@@ -11,8 +11,23 @@ with open(os.path.join('skglm', '__init__.py'), 'r') as fid:
 if version is None:
     raise RuntimeError('Could not determine version')
 
-setup(name="skglm",
+DISTNAME = 'skglm'
+DESCRIPTION = 'Fast and modular scikit-learn replacement for generalized linear models'
+MAINTAINER = 'Mathurin Massias'
+MAINTAINER_EMAIL = 'mathurin.massias@gmail.com'
+LICENSE = 'BSD (3-clause)'
+DOWNLOAD_URL = 'https://github.com/scikit-learn-contrib/skglm.git'
+VERSION = version
+URL = 'https://contrib.scikit-learn.org/skglm'
+
+setup(name=DISTNAME,
       version=version,
+      description=DESCRIPTION,
+      long_description=open('README.rst').read(),
+      maintainer=MAINTAINER,
+      maintainer_email=MAINTAINER_EMAIL,
+      url=URL,
+      download_url=DOWNLOAD_URL,
       packages=find_packages(),
       install_requires=['libsvmdata>=0.2', 'numpy>=1.12', 'numba',
                         'seaborn>=0.7',

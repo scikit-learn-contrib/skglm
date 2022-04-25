5c0a33a8ea7f39152e7b9f47845b2e7c1c18b2b0
pabannier <pierreantoine.bannier@gmail.com>
2022-04-25 10:54:36 +0200

    example with weights

--- gram_test.py
+++ gram_test.py
@@ -12,19 +12,19 @@ X = np.load("design_matrix.npy")
 y = np.load("target.npy")
 groups = np.load("groups.npy")
 weights = np.load("weights.npy")
-# grps = [list(np.where(groups == i)[0]) for i in range(1, 33)]
+grps = [list(np.where(groups == i)[0]) for i in range(1, 33)]
 
 
 alpha_ratio = 1e-2
 n_alphas = 10
-
+tol = 1e-8
 
 # Case 1: slower runtime for (very) small alphas
 # alpha_max = 0.003471727067743962
 alpha_max = np.max(np.linalg.norm((X.T @ y).reshape(-1, 5), axis=1)) / len(y)
 alpha = alpha_max / 100
-clf = GroupLasso(fit_intercept=False,
-                 groups=5, alpha=alpha, verbose=1)
+clf = GroupLasso(fit_intercept=False, tol=tol,
+                 groups=grps, weights=weights, alpha=alpha, verbose=1)
 
 t0 = time.time()
 clf.fit(X, y)
@@ -32,12 +32,9 @@ t1 = time.time()
 
 print(f"Celer: {t1 - t0:.3f} s")
 
-# beware: stopping criterion is not the same, tol here needs to be lower
-# to get meaningful comparison
 t0 = time.time()
-res = group_lasso(X, y, alpha, groups=5, tol=1e-10, max_iter=10_000, check_freq=10)
+res = gram_group_lasso(X, y, alpha, groups=grps, tol=tol, weights=weights, max_iter=10_000,
+                       check_freq=50)
 t1 = time.time()
 
 print(f"skglm gram: {t1 - t0:.3f} s")
-
-# TODO support weights in gram solver

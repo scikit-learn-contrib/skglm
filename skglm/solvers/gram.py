f142317e8ed727263f363ad0bc60432d910505e7
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2022-04-25 08:50:02 +0200

    duality gap for BCD

--- skglm/solvers/gram.py
+++ skglm/solvers/gram.py
@@ -13,6 +13,15 @@ def primal(alpha, r, w, weights):
 
 
 @njit
+def primal_grp(alpha, norm_r2, r, w, grp_ptr, grp_indices, weights):
+    p_obj = norm_r2 / (2 * len(r))
+    for g in range(len(grp_ptr) - 1):
+        w_g = w[grp_indices[grp_ptr[g]:grp_ptr[g + 1]]]
+        p_obj += alpha * norm(w_g * weights[g], ord=2)
+    return p_obj
+
+
+@njit
 def dual(alpha, norm_y2, theta, y):
     d_obj = - np.sum((y / (alpha * len(y)) - theta) ** 2)
     d_obj *= 0.5 * alpha ** 2 * len(y)
@@ -31,9 +40,34 @@ def dnorm_l1(theta, X, weights):
 
 
 @njit
+def dnorm_l21(theta, grp_ptr, grp_indices, X, weights):
+    scal = 0.
+    n_groups = len(grp_ptr) - 1
+    for g in range(n_groups):
+        if weights[g] == np.inf:
+            continue
+        tmp = 0.
+        for k in range(grp_ptr[g], grp_ptr[g + 1]):
+            j = grp_indices[k]
+            Xj_theta = X[:, j] @ theta
+            tmp += Xj_theta ** 2
+        scal = max(scal, np.sqrt(tmp) / weights[g])
+    return scal
+
+
+@njit
 def create_dual_point(r, alpha, X, y, weights):
     theta = r / (alpha * len(y))
     scal = dnorm_l1(theta, X, weights)
+    if scal > 1.:
+        theta /= scal
+    return theta
+
+
+@njit
+def create_dual_point_grp(r, alpha, y, X, grp_ptr, grp_indices, weights):
+    theta = r / (alpha * len(y))
+    scal = dnorm_l21(theta, grp_ptr, grp_indices, X, weights)
     if scal > 1.:
         theta /= scal
     return theta
@@ -49,13 +83,13 @@ def dual_gap(alpha, norm_y2, y, X, w, weights):
 
 
 @njit
-def primal_grp(alpha, y, X, w, grp_ptr, grp_indices, weights):
+def dual_gap_grp(y, X, w, alpha, norm_y2, grp_ptr, grp_indices, weights):
     r = y - X @ w
-    p_obj = (r @ r) / (2 * len(y))
-    for g in range(len(grp_ptr) - 1):
-        w_g = w[grp_indices[grp_ptr[g]:grp_ptr[g + 1]]]
-        p_obj += alpha * norm(w_g * weights[g], ord=2)
-    return p_obj
+    norm_r2 = r @ r
+    p_obj = primal_grp(alpha, norm_r2, r, w, grp_ptr, grp_indices, weights)
+    theta = create_dual_point_grp(r, alpha, y, X, grp_ptr, grp_indices, weights)
+    d_obj = dual(alpha, norm_y2, theta, y)
+    return p_obj, d_obj, p_obj - d_obj
 
 
 @njit
@@ -133,8 +167,8 @@ def gram_fista_group_lasso(X, y, alpha, groups, max_iter, tol, w_init=None,
 
     w = w_init.copy() if w_init is not None else np.zeros(n_features)
     z = w_init.copy() if w_init is not None else np.zeros(n_features)
-    weights = weights if weights is not None else np.ones(n_features)
-    tiled_weights = np.repeat(weights, grp_size)
+    weights = weights if weights is not None else np.ones(n_groups)
+    # tiled_weights = np.repeat(weights, grp_size)
 
     G = X.T @ X
     Xty = X.T @ y
@@ -150,10 +184,10 @@ def gram_fista_group_lasso(X, y, alpha, groups, max_iter, tol, w_init=None,
         t_new = (1 + np.sqrt(1 + 4 * t_old ** 2)) / 2
         w_old = w.copy()
         z -= (G @ z - Xty) / tiled_lipschitz / len(y)
-        w = BST_vec(z, alpha / tiled_lipschitz * tiled_weights, n_features)
+        w = BST_vec(z, alpha / lipschitz, grp_size)
         z = w + (t_old - 1.) / t_new * (w - w_old)
         if n_iter % check_freq == 0:
-            p_obj = primal_grp(alpha, y, X, w, grp_ptr, grp_indices, weights)
+            p_obj = primal_grp(alpha, y, X, w, grp_ptr, grp_indices, np.ones(n_groups))
             print(f"iter {n_iter} :: p_obj {p_obj:.5f}")
             if p_obj_prev - p_obj < tol:
                 print("Convergence reached!")
@@ -164,10 +198,10 @@ def gram_fista_group_lasso(X, y, alpha, groups, max_iter, tol, w_init=None,
 
 def gram_group_lasso(X, y, alpha, groups, max_iter, tol, w_init=None, weights=None,
                      check_freq=50):
-    p_obj_prev = np.inf
     n_features = X.shape[1]
     grp_ptr, grp_indices = _grp_converter(groups, X.shape[1])
     n_groups = len(grp_ptr) - 1
+    norm_y2 = y @ y
     grads = X.T @ y / len(y)
     G = X.T @ X
     lipschitz = np.zeros(n_groups, dtype=X.dtype)
@@ -180,12 +214,13 @@ def gram_group_lasso(X, y, alpha, groups, max_iter, tol, w_init=None, weights=No
     for n_iter in range(max_iter):
         bcd_epoch(X, G, grads, w, alpha, lipschitz, grp_indices, grp_ptr, weights)
         if n_iter % check_freq == 0:
-            p_obj = primal_grp(alpha, y, X, w, grp_ptr, grp_indices, weights)
-            if p_obj_prev - p_obj < tol:
+            p_obj, d_obj, d_gap = dual_gap_grp(y, X, w, alpha, norm_y2, grp_ptr,
+                                               grp_indices, weights)
+            print(f"iter {n_iter} :: p_obj {p_obj:.5f} :: d_obj {d_obj:.5f} " +
+                  f":: gap {d_gap:.5f}")
+            if d_gap < tol:
                 print("Convergence reached!")
                 break
-            print(f"iter {n_iter} :: p_obj {p_obj}")
-            p_obj_prev = p_obj
     return w
 
 

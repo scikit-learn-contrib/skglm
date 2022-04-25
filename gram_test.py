ae92334d2b685e1189dc04fa1285e33b715e9894
pabannier <pierreantoine.bannier@gmail.com>
2022-04-25 11:03:22 +0200

    ENH fista to example

--- gram_test.py
+++ gram_test.py
@@ -2,11 +2,9 @@
 
 
 import time
-from numpy.linalg import norm
-import matplotlib.pyplot as plt
 import numpy as np
 from celer import GroupLasso
-from skglm.solvers.gram import gram_group_lasso
+from skglm.solvers.gram import gram_fista_group_lasso, gram_group_lasso
 
 X = np.load("design_matrix.npy")
 y = np.load("target.npy")
@@ -19,7 +17,7 @@ alpha_ratio = 1e-2
 n_alphas = 10
 tol = 1e-8
 
-# Case 1: slower runtime for (very) small alphas
+# Case 1: slower runtime for small alphas
 # alpha_max = 0.003471727067743962
 alpha_max = np.max(np.linalg.norm((X.T @ y).reshape(-1, 5), axis=1)) / len(y)
 alpha = alpha_max / 100
@@ -38,3 +36,21 @@ res = gram_group_lasso(X, y, alpha, groups=grps, tol=tol, weights=weights, max_i
 t1 = time.time()
 
 print(f"skglm gram: {t1 - t0:.3f} s")
+
+
+# FISTA Gram for very small alphas
+alpha = alpha_max / 1e-4
+clf = GroupLasso(fit_intercept=False, tol=tol, groups=grps, weights=weights, alpha=alpha, 
+                 verbose=1)
+
+t0 = time.time()
+clf.fit(X, y)
+t1 = time.time()
+
+print(f"Celer: {t1 - t0:.3f} s")
+
+t0 = time.time()
+res = gram_fista_group_lasso(X, y, alpha, groups=grps, tol=tol, weights=weights, max_iter=10_000,
+                             check_freq=50)
+t1 = time.time()
+print(f"skglm fista gram: {t1 - t0:.3f} s")

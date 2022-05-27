b278ec1c7859095d810c46a2d0c140b91d24ed05
Badr MOUFAD <Badr.MOUFAD@emines.um6p.ma>
2022-05-28 00:21:58 +0200

    implement ws [working]

--- skglm/solvers/group_cd.py
+++ skglm/solvers/group_cd.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numpy.linalg import norm
-from numba import njit
+from numba import njit, jit
 
 from skglm.datafits.base import BaseMultitaskDatafit
 from skglm.penalties.base import BasePenalty
@@ -9,6 +9,7 @@ from skglm.penalties.block_separable import SparseGroupL1
 from skglm.datafits.multi_task import QuadraticGroup
 
 from skglm.utils import make_correlated_data, grp_converter
+from celer import GroupLasso
 
 
 FREQ_CHECK = 10
@@ -23,7 +24,6 @@ def group_solver(X: np.ndarray, y: np.ndarray,
     n_features: int = X.shape[1]
     grp_ptr, grp_indices = penalty.grp_ptr, penalty.grp_indices
     n_groups: int = len(grp_ptr) - 1
-    ws_size: int = 0
 
     # init
     w = np.zeros(n_features, dtype=np.float64)
@@ -34,19 +34,18 @@ def group_solver(X: np.ndarray, y: np.ndarray,
         ws = np.arange(n_groups)
         is_optimal, scores = _check_is_optimal(datafit, penalty, y, X, w, Xw,
                                                ws, stop_tol)
+
         if is_optimal:
             print("Outer Solver: early exit")
             break
 
-        ws_size = max(p0,
-                      min(n_features, 2 * penalty.generalized_support(w)))
-        ws = np.argpartition(scores, -ws_size)[-ws_size:]
+        ws, _ = _select_ws(w, penalty, scores, p0)
 
         if verbose == 1:
             p_obj = datafit.value(y, w, Xw) + penalty.value(w)
             print(f"Iteration {k}: {p_obj}")
 
-        for epoch in np.arange(1, max_epochs+1):
+        for epoch in range(1, max_epochs+1):
             # inplace update
             _cycle_group_cd(datafit, penalty, y, X,
                             w, Xw, grp_ptr, grp_indices, ws)
@@ -67,7 +66,7 @@ def group_solver(X: np.ndarray, y: np.ndarray,
 @njit
 def _cycle_group_cd(datafit, penalty, y, X, w, Xw, grp_ptr, grp_indices, ws):
 
-    for g in range(ws):
+    for g in ws:
         grp_g_indices = grp_indices[grp_ptr[g]:grp_ptr[g+1]]
         X_g = X[:, grp_g_indices]
         old_w_g = w[grp_g_indices].copy()
@@ -79,19 +78,54 @@ def _cycle_group_cd(datafit, penalty, y, X, w, Xw, grp_ptr, grp_indices, ws):
             old_w_g - inv_lipschitz_g * grad_g_datafit,
             inv_lipschitz_g, g)
 
-        if w[grp_g_indices] != old_w_g:
+        if norm(w[grp_g_indices] - old_w_g) != 0:
             Xw += X_g @ (w[grp_g_indices] - old_w_g)
 
 
 @njit
 def _check_is_optimal(datafit, penalty, y, X, w, Xw, ws, stop_tol):
-    grad = np.vstack((datafit.gradient_j(X, y, w, Xw, g)
-                      for g in ws))
+    grad = []
+    for g in ws:
+        grad.append(datafit.gradient_j(X, y, w, Xw, g))
+
     scores = penalty.subdiff_distance(w, grad, ws)
     is_optimal = np.max(scores) < stop_tol
 
     return is_optimal, scores
 
 
+@njit
+def _select_ws(w, penalty, scores, p0):
+    n_features = w.shape[0]
+    size_support = penalty.generalized_support(w).sum()
+
+    ws_size = max(p0,
+                  min(n_features, 2 * size_support))
+
+    ws = np.zeros(ws_size, np.int32)
+    for i in range(ws_size):
+        ws[i] = np.argmax(scores)
+        np.delete(scores, ws[i])
+
+    return ws, ws_size
+
+
 if __name__ == '__main__':
+    n_samples, n_features = 1000, 100
+    groups = 100  # contiguous groups of 100 features
+    X, y, _ = make_correlated_data(n_samples, n_features, random_state=42)
+    grp_ptr, grp_indices = grp_converter(groups, n_features)
+
+    n_groups = n_features // groups
+    weights = np.ones(n_groups)
+
+    alpha_max = norm(X.T @ y / np.repeat(weights, groups), ord=np.inf) / n_samples
+    alpha = alpha_max / 10.
+
+    group_penalty = SparseGroupL1(
+        alpha=alpha, tau=0., weights=weights, grp_ptr=grp_ptr, grp_indices=grp_indices)
+
+    group_datafit = QuadraticGroup(grp_ptr, grp_indices)
+
+    group_solver(X, y, group_datafit, group_penalty, verbose=1)
     pass

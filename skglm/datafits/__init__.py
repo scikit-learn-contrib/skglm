a05e578959fa4970f198bba78706d215f50de5b3
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2022-06-15 11:17:22 +0200

    linter happy

--- skglm/datafits/__init__.py
+++ skglm/datafits/__init__.py
@@ -1,7 +1,8 @@
 from .base import BaseDatafit, BaseMultitaskDatafit  # noqa F401
 
 from .single_task import (  # noqa F401
-    Quadratic, Quadratic_32, QuadraticSVC, QuadraticSVC_32, Logistic, Logistic_32, Huber, Huber_32,
+    Quadratic, Quadratic_32, QuadraticSVC, QuadraticSVC_32, Logistic, Logistic_32,
+    Huber, Huber_32,
 )
 
 from .multi_task import QuadraticMultiTask  # noqa F401

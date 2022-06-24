e3d90b35673bf29c337828c79a7f2b09a9d544bc
Badr MOUFAD <Badr.MOUFAD@emines.um6p.ma>
2022-06-24 15:16:32 +0200

    clean ups

--- benchmark_entry.py
+++
@@ -1,6 +0,0 @@
-from benchopt.benchmark import Benchmark
-from benchopt import run_benchmark
-
-bench_aa = Benchmark('./benchmark_aa_single')
-
-run_benchmark(bench_aa, max_runs=25, n_jobs=1, n_repetitions=1)

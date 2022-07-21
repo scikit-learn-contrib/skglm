8cbcb9acc0e07dd5105d941a9294dde260c62937
Klopfe <quentin.klopfenstein@u-bourgogne.fr>
2022-07-21 10:39:05 +0200

    unit testing huber loss

--- skglm/tests/test_datafits.py
+++ skglm/tests/test_datafits.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 
 from sklearn.linear_model import HuberRegressor
 from numpy.testing import assert_allclose, assert_array_less
@@ -9,12 +10,13 @@ from skglm import GeneralizedLinearEstimator
 from skglm.utils import make_correlated_data
 
 
-def test_huber_datafit():
+@pytest.mark.parametrize('fit_intercept', [False, True])
+def test_huber_datafit(fit_intercept):
     # test only datafit: there does not exist other implems with sparse penalty
     X, y, _ = make_correlated_data(n_samples=20, n_features=10, random_state=0)
     # disable L2^2 regularization (alpha=0)
     their = HuberRegressor(
-        fit_intercept=False, alpha=0, tol=1e-12, epsilon=1.35
+        fit_intercept=fit_intercept, alpha=0, tol=1e-12, epsilon=1.35
     ).fit(X, y)
 
     # sklearn optimizes over a scale, we must match delta:
@@ -24,7 +26,7 @@ def test_huber_datafit():
     ours = GeneralizedLinearEstimator(
         datafit=Huber(delta),
         penalty=WeightedL1(1, np.zeros(X.shape[1])),
-        tol=1e-14,
+        tol=1e-14, fit_intercept=fit_intercept
     ).fit(X, y)
 
     assert_allclose(ours.coef_, their.coef_, rtol=1e-3)

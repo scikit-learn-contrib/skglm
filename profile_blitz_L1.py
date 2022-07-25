07182933d0c26f0318d0be84a9ac473923a99a99
QB3 <quentin.bertrand@inria.fr>
2022-07-25 09:41:35 -0400

    [ci skip] update profile

--- profile_blitz_L1.py
+++ profile_blitz_L1.py
@@ -8,13 +8,14 @@ from skglm.solvers.prox_newton_solver import prox_newton_solver
 from skglm.datafits import Logistic
 from skglm.penalties import L1
 
-n_samples, n_features = 100, 3000
+n_samples, n_features = 1_000, 10_000
 
 rng = check_random_state(0)
 X = rng.normal(0, 1, (n_samples, n_features))
 y = np.sign(rng.normal(0, 1, (n_samples,)))
 
-alpha = np.linalg.norm(X.T @ y, ord=np.inf) * 0.1 / 2
+alpha_max = np.linalg.norm(X.T @ y, ord=np.inf) / 2
+alpha = alpha_max / 100
 
 tol = 1e-10
 
@@ -25,19 +26,19 @@ blitzl1.set_verbose(True)
 prob = blitzl1.LogRegProblem(X, y)
 sol = prob.solve(alpha, p0=n_features)
 
-df = Logistic()
-df.initialize(X, y)
+# df = Logistic()
+# df.initialize(X, y)
 
-pen = L1(alpha / n_samples)
-# max_iter = 1
-# max_epochs = 1
-# max_pn_cd_epochs = 2
-eps_in = 0.3
+# pen = L1(alpha / n_samples)
+# # max_iter = 1
+# # max_epochs = 1
+# # max_pn_cd_epochs = 2
+# eps_in = 0.3
 
 
-w = np.zeros(n_features)
-Xw = np.zeros(n_samples)
-prox_newton_solver(
-    X, y, df, pen, w, Xw, tol=tol, p0=n_features, verbose=100, eps_in=eps_in)
+# w = np.zeros(n_features)
+# Xw = np.zeros(n_samples)
+# prox_newton_solver(
+#     X, y, df, pen, w, Xw, tol=tol, p0=n_features, verbose=100, eps_in=eps_in)
 
-np.testing.assert_allclose(sol.x, w, rtol=1e-5)
+# np.testing.assert_allclose(sol.x, w, rtol=1e-5)

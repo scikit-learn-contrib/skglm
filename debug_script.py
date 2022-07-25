77bb6d04bf8b073253231e5bc45b31792ffb540f
Badr MOUFAD <Badr.MOUFAD@emines.um6p.ma>
2022-07-25 17:30:17 +0200

    ``spec_to_float32`` fails in non float attributes

--- debug_script.py
+++ debug_script.py
@@ -1,16 +1,29 @@
 import numpy as np
-from skglm.datafits import Quadratic, Quadratic_32
+from skglm.datafits import Quadratic
 from skglm.utils import make_correlated_data
 
+from numba import int32, float64
+from skglm.datafits.base import spec_to_float32
 
-X, y, _ = make_correlated_data()
 
-X = X.astype(np.float32)
-y = y.astype(np.float32)
+# X, y, _ = make_correlated_data()
 
-print(X.dtype, y.dtype)
+# X = X.astype(np.float32)
+# y = y.astype(np.float32)
 
-# This will throw an error
-# one should use Quadratic_32 instead
-quad = Quadratic()
-quad.initialize(X, y)
+# print(X.dtype, y.dtype)
+
+# # This will throw an error
+# # one should use Quadratic_32 instead
+# quad = Quadratic()
+# quad.initialize(X, y)
+
+
+spec_QuadraticGroup = [
+    ('grp_ptr', int32[:]),
+    ('grp_indices', int32[:]),
+    ('lipschitz', float64[:])
+]
+
+# int are converted to float
+print(spec_to_float32(spec_QuadraticGroup))

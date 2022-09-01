5ff9945f4fdbddae29908133b8b43fff11802d64
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2022-09-01 12:55:04 +0200

    fix test datafits

--- skglm/tests/test_penalties.py
+++ skglm/tests/test_penalties.py
@@ -9,6 +9,7 @@ from skglm.penalties import (
     L1, L1_plus_L2, WeightedL1, MCPenalty, SCAD, IndicatorBox, L0_5, L2_3,
     L2_1, L2_05, BlockMCPenalty, BlockSCAD)
 from skglm import GeneralizedLinearEstimator
+from skglm.solvers import AcceleratedCD
 from skglm.utils import make_correlated_data
 
 
@@ -49,7 +50,7 @@ def test_subdiff_diff(penalty):
     est = GeneralizedLinearEstimator(
         datafit=Quadratic(),
         penalty=penalty,
-        tol=tol,
+        solver=AcceleratedCD(tol=tol)
     ).fit(X, y)
     # assert the stopping criterion is satisfied
     assert_array_less(est.stop_crit_, tol)

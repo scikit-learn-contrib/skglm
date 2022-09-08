38bf792452e076272da44b1fd0ef921d0ad7d147
mathurinm <mathurin.massias@gmail.com>
2022-09-08 08:39:10 +0200

    CLN

--- skglm/tests/test_datafits.py
+++ skglm/tests/test_datafits.py
@@ -58,23 +58,3 @@ def test_log_datafit():
 
 if __name__ == '__main__':
     pass
-    fit_intercept = True
-    X, y, _ = make_correlated_data(n_samples=20, n_features=10, random_state=0)
-    # disable L2^2 regularization (alpha=0)
-    their = HuberRegressor(
-        fit_intercept=fit_intercept, alpha=0, tol=1e-12, epsilon=1.35
-    ).fit(X, y)
-
-    # sklearn optimizes over a scale, we must match delta:
-    delta = their.epsilon * their.scale_
-
-    # TODO we should have an unpenalized solver
-    ours = GeneralizedLinearEstimator(
-        datafit=Huber(delta),
-        penalty=WeightedL1(1, np.zeros(X.shape[1])),
-        solver=AndersonCD(tol=1e-14, fit_intercept=fit_intercept),
-    ).fit(X, y)
-
-    assert_allclose(ours.coef_, their.coef_, rtol=1e-3)
-    assert_allclose(ours.intercept_, their.intercept_, rtol=1e-4)
-    assert_array_less(ours.stop_crit_, ours.solver.tol)

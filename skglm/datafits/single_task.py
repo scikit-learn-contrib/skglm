9d2c04363087be296fdd3c580dbca4d7318249c8
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2022-10-09 21:18:12 +0200

    add docstrings

--- skglm/datafits/single_task.py
+++ skglm/datafits/single_task.py
@@ -363,6 +363,17 @@ class Huber(BaseDatafit):
 
 
 class Poisson(BaseDatafit):
+    """Poisson datafit.
+
+    The datafit reads::
+
+    (1 / n_samples) * \sum_i (exp(Xw_i) - y_i * Xw_i)
+
+    Note
+    ----
+    The class is jit compiled at fit time using Numba compiler.
+    This allows for faster computations.
+    """
     def __init__(self):
         pass
 

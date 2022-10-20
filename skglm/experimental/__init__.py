fe6597797290826ee8b6983a680850146f9f04a3
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2022-10-20 18:59:53 +0200

    rm private functions

--- skglm/experimental/__init__.py
+++ skglm/experimental/__init__.py
@@ -1,9 +1,7 @@
-from .reweighted import IterativeReweightedL1, _L23_objective, _L23_derivative
+from .reweighted import IterativeReweightedL1
 from .sqrt_lasso import SqrtLasso
 
 __all__ = [
     SqrtLasso,
     IterativeReweightedL1,
-    _L23_objective,
-    _L23_derivative
 ]

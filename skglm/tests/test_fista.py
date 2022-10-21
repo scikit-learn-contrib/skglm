2bbc8f5394bccae0ce896d2a3dd8870409a337a1
Badr MOUFAD <Badr.MOUFAD@emines.um6p.ma>
2022-10-21 11:46:13 +0200

    fix && refactor unittest

--- skglm/tests/test_fista.py
+++ skglm/tests/test_fista.py
@@ -2,23 +2,27 @@ import pytest
 
 import numpy as np
 from numpy.linalg import norm
+
+import scipy.sparse
 from scipy.sparse import csc_matrix, issparse
 
-from skglm.datafits import Quadratic, Logistic, QuadraticSVC
 from skglm.penalties import L1, IndicatorBox
 from skglm.solvers import FISTA, AndersonCD
+from skglm.datafits import Quadratic, Logistic, QuadraticSVC
 from skglm.utils import make_correlated_data, compiled_clone, spectral_norm2
 
 
-np.random.seed(0)
+random_state = 113
 n_samples, n_features = 50, 60
-X, y, _ = make_correlated_data(
-    n_samples=n_samples, n_features=n_features, random_state=0)
-X_sparse = csc_matrix(X * np.random.binomial(1, 0.1, X.shape))
+
+rng = np.random.RandomState(random_state)
+X, y, _ = make_correlated_data(n_samples, n_features, random_state=rng)
+rng.seed(random_state)
+X_sparse = csc_matrix(X * np.random.binomial(1, 0.5, X.shape))
 y_classif = np.sign(y)
 
 alpha_max = norm(X.T @ y, ord=np.inf) / len(y)
-alpha = alpha_max / 100
+alpha = alpha_max / 10
 
 tol = 1e-10
 
@@ -40,20 +44,23 @@ def test_fista_solver(X, Datafit, Penalty):
     penalty = compiled_clone(Penalty(alpha))
 
     solver = FISTA(max_iter=1000, tol=tol)
-    res_fista = solver.solve(X, _y, datafit, penalty)
+    w_fista = solver.solve(X, _y, datafit, penalty)[0]
 
     solver_cd = AndersonCD(tol=tol, fit_intercept=False)
-    res_cd = solver_cd.solve(X, _y, datafit, penalty)
+    w_cd = solver_cd.solve(X, _y, datafit, penalty)[0]
 
-    np.testing.assert_allclose(res_fista[0], res_cd[0], rtol=1e-3)
+    np.testing.assert_allclose(w_fista, w_cd, atol=1e-7)
 
 
 def test_spectral_norm2():
-    X_bundles = (X_sparse.data, X_sparse.indptr, X_sparse.indices)
-    lipschitz_our = spectral_norm2(*X_bundles, n_samples=len(y))
-    lipschitz_np = norm(X_sparse.toarray(), ord=2) ** 2
+    n_samples, n_features = 50, 60
+    A_sparse = scipy.sparse.random(n_samples, n_features, density=0.5, format='csc')
 
-    np.testing.assert_allclose(lipschitz_our, lipschitz_np, atol=1e-4, rtol=1e-5)
+    A_bundles = (A_sparse.data, A_sparse.indptr, A_sparse.indices)
+    lipschitz_our = spectral_norm2(*A_bundles, n_samples=len(y))
+    lipschitz_np = norm(A_sparse.toarray(), ord=2) ** 2
+
+    np.testing.assert_allclose(lipschitz_our, lipschitz_np)
 
 
 if __name__ == '__main__':

3668e05a7ccb2006b8c5ccc3775345a73b7b507a
Badr-MOUFAD <badr.moufad@emines.um6p.ma>
2022-10-27 13:46:58 +0200

    create ``non_seperable`` module

--- skglm/penalties/__init__.py
+++ skglm/penalties/__init__.py
@@ -1,14 +1,16 @@
 from .base import BasePenalty
 from .separable import (
-    L1_plus_L2, L0_5, L1, L2_3, MCPenalty, SCAD, WeightedL1, IndicatorBox, SLOPE
+    L1_plus_L2, L0_5, L1, L2_3, MCPenalty, SCAD, WeightedL1, IndicatorBox
 )
 from .block_separable import (
     L2_05, L2_1, BlockMCPenalty, BlockSCAD, WeightedGroupL2
 )
 
+from .non_seperable import SLOPE
+
 
 __all__ = [
     BasePenalty,
-    L1_plus_L2, L0_5, L1, L2_3, MCPenalty, SCAD, WeightedL1, IndicatorBox, SLOPE,
-    L2_05, L2_1, BlockMCPenalty, BlockSCAD, WeightedGroupL2
+    L1_plus_L2, L0_5, L1, L2_3, MCPenalty, SCAD, WeightedL1, IndicatorBox,
+    L2_05, L2_1, BlockMCPenalty, BlockSCAD, WeightedGroupL2, SLOPE
 ]

92c2a456b360a923f4cb961cea2a97c417fc13c2
Klopfe <40163547+Klopfe@users.noreply.github.com>
2022-10-27 13:36:33 +0200

    [WIP] Doc - Intercept computation (#84)
    Co-authored-by: mathurinm <mathurin.massias@gmail.com>

--- doc/conf.py
+++ doc/conf.py
@@ -58,9 +58,15 @@ extensions = [
     'sphinx.ext.autosectionlabel',
     'numpydoc',
     'sphinx.ext.linkcode',
-    'gh_substitutions',  # custom ext, see ./sphinxext/gh_substitutions.py
+    'gh_substitutions',
+    'myst_parser',
+    # custom ext, see ./sphinxext/gh_substitutions.py
 ]
 
+myst_enable_extensions = [
+    "dollarmath",
+    "amsmath"
+]
 # generate autosummary even if no references
 autosummary_generate = True
 

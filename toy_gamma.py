8219775e6a200a020bbb2c04adf7514a275b483a
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2022-10-28 23:05:31 +0200

    for mathurin

--- toy_gamma.py
+++ toy_gamma.py
@@ -13,26 +13,25 @@ n_samples = 10
 n_features = 20
 X, y, _ = make_correlated_data(n_samples, n_features, random_state=0)
 y[y <= 0] = 0.1
-alpha_max = norm(X.T @ y, ord=np.inf) / n_samples 
-alpha = alpha_max * 0.05
 alpha = 0
-
 
 tol = 1e-12
 
-clf_sk = GammaRegressor(alpha=alpha, fit_intercept=False).fit(X, y)
+clf_sk = GammaRegressor(alpha=alpha * n_samples, fit_intercept=False).fit(X, y)
 
 gamma_model = sm.GLM(y, X, family=sm.families.Gamma(sm.families.links.Log()))
-gamma_results = gamma_model.fit()
-# gamma_results = gamma_model.fit_regularized(
-#         method="elastic_net", L1_wt=1, cnvrg_tol=tol, alpha=alpha)
+gamma_results = gamma_model.fit_regularized(
+        method="elastic_net", L1_wt=1, cnvrg_tol=tol, alpha=alpha * n_samples)
 
-np.testing.assert_allclose(clf_sk.coef_, gamma_results.params, rtol=1e-5)
+clf = GeneralizedLinearEstimator(
+    datafit=Gamma(),
+    penalty=L1(alpha),
+    solver=ProxNewton(fit_intercept=False, verbose=2, tol=tol)
+).fit(X, y)
 
-# clf = GeneralizedLinearEstimator(
-#     datafit=Gamma(),
-#     penalty=L1(alpha),
-#     solver=ProxNewton(fit_intercept=False, verbose=2, tol=tol)
-# ).fit(X, y)
+print("sklearn:", norm(clf_sk.coef_))
+print("skglm:", norm(clf.coef_))
+print("sm:", norm(gamma_results.params))
 
+# np.testing.assert_allclose(clf_sk.coef_, gamma_results.params, rtol=1e-5)
 # np.testing.assert_allclose(clf.coef_, gamma_results.params, rtol=1e-5)

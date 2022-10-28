cd1d28b24f14856b8e6ad22fc81a64f5e88c59b7
mathurinm <mathurin.massias@gmail.com>
2022-10-29 00:14:55 +0200

    use fat data

--- toy_gamma.py
+++ toy_gamma.py
@@ -9,8 +9,8 @@ from sklearn.linear_model import GammaRegressor
 import statsmodels.api as sm
 
 
-n_samples = 10
-n_features = 20
+n_samples = 100
+n_features = 40
 X, y, _ = make_correlated_data(n_samples, n_features, random_state=0)
 y[y <= 0] = 0.1
 alpha = 0
@@ -21,7 +21,7 @@ clf_sk = GammaRegressor(alpha=alpha * n_samples, fit_intercept=False).fit(X, y)
 
 gamma_model = sm.GLM(y, X, family=sm.families.Gamma(sm.families.links.Log()))
 gamma_results = gamma_model.fit_regularized(
-        method="elastic_net", L1_wt=1, cnvrg_tol=tol, alpha=alpha * n_samples)
+    method="elastic_net", L1_wt=1, cnvrg_tol=tol, alpha=alpha * n_samples)
 
 clf = GeneralizedLinearEstimator(
     datafit=Gamma(),
@@ -29,9 +29,11 @@ clf = GeneralizedLinearEstimator(
     solver=ProxNewton(fit_intercept=False, verbose=2, tol=tol)
 ).fit(X, y)
 
-print("sklearn:", norm(clf_sk.coef_))
-print("skglm:", norm(clf.coef_))
-print("sm:", norm(gamma_results.params))
+
+print(norm(clf.coef_ - clf_sk.coef_))
+# print("sklearn:", (clf_sk.coef_[:10]))
+# print("skglm:", (clf.coef_))
+# print("sm:", (gamma_results.params))
 
 # np.testing.assert_allclose(clf_sk.coef_, gamma_results.params, rtol=1e-5)
 # np.testing.assert_allclose(clf.coef_, gamma_results.params, rtol=1e-5)

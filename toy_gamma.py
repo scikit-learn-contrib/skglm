babee656cb6b6c43c06eea474805604021c62dad
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2022-10-30 13:41:54 +0100

    wip doc

--- toy_gamma.py
+++ toy_gamma.py
@@ -5,7 +5,7 @@ from skglm.penalties import L1
 from skglm.datafits import Gamma
 from skglm.solvers import ProxNewton
 from skglm.estimators import GeneralizedLinearEstimator
-from sklearn.linear_model import GammaRegressor
+# from sklearn.linear_model import GammaRegressor
 import statsmodels.api as sm
 
 
@@ -13,11 +13,13 @@ n_samples = 100
 n_features = 40
 X, y, _ = make_correlated_data(n_samples, n_features, random_state=0)
 y[y <= 0] = 0.1
+alpha_max = norm(X.T @ y, ord=np.inf) / n_samples
+alpha = alpha_max * 0.01 
 alpha = 0
 
 tol = 1e-12
 
-clf_sk = GammaRegressor(alpha=alpha * n_samples, fit_intercept=False).fit(X, y)
+# clf_sk = GammaRegressor(alpha=alpha * n_samples, fit_intercept=False).fit(X, y)
 
 gamma_model = sm.GLM(y, X, family=sm.families.Gamma(sm.families.links.Log()))
 gamma_results = gamma_model.fit_regularized(
@@ -30,7 +32,9 @@ clf = GeneralizedLinearEstimator(
 ).fit(X, y)
 
 
-print(norm(clf.coef_ - clf_sk.coef_))
+np.testing.assert_allclose(clf.coef_, gamma_results.params, rtol=1e-5)
+
+# print(norm(clf.coef_ - clf_sk.coef_))
 # print("sklearn:", (clf_sk.coef_[:10]))
 # print("skglm:", (clf.coef_))
 # print("sm:", (gamma_results.params))

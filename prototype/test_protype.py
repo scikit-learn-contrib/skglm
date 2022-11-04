8a6f61fdacb593eeb144586d02e7b10fc8b0e558
Badr-MOUFAD <badr.moufad@emines.um6p.ma>
2022-11-04 14:12:18 +0100

    test solvers against sklearn

---
+++ prototype/test_protype.py
@@ -0,0 +1,30 @@
+import pytest
+
+import numpy as np
+from numpy.linalg import norm
+
+from sklearn.linear_model import Lasso
+from skglm.utils import make_correlated_data
+
+from prototype.pd_lasso import cp_lasso, fb_lasso
+
+
+@pytest.mark.parametrize('solver', [fb_lasso, cp_lasso])
+def test_solver(solver):
+    rho = 0.1
+    n_samples, n_features = 50, 10
+    A, b, _ = make_correlated_data(n_samples, n_features,
+                                   random_state=0)
+
+    alpha_max = norm(A.T @ b, ord=np.inf) / 2
+    alpha = rho * alpha_max
+
+    w, _ = solver(A, b, alpha, max_iter=1000)
+    lasso = Lasso(fit_intercept=False,
+                  alpha=alpha / n_samples).fit(A, b)
+
+    np.testing.assert_allclose(w, lasso.coef_.flatten(), atol=1e-4)
+
+
+if __name__ == '__main__':
+    pass

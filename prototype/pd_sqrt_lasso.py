c05456d4a210554c26b4b33a8f9736c567e09776
Badr-MOUFAD <badr.moufad@emines.um6p.ma>
2022-11-10 11:16:06 +0100

    un normalized sqrt lasso

---
+++ prototype/pd_sqrt_lasso.py
@@ -0,0 +1,111 @@
+"""Solve an un-normalized Lasso problem using P-D method
+
+Problem reads::
+
+    min_x ||b - Ax|| + alpha ||x||_1
+
+Which can be recast as a saddle point problem::
+
+    max_y min_x <Ax, y> + g(x) - h*(y)
+
+where::
+
+    g(x) = alpha ||x||_1
+    h(z) = ||b - z||  <===>  h*(y) = <y, b> + ||.||^*(-y)
+"""
+
+import numpy as np
+from numpy.linalg import norm
+
+from skglm.utils import ST, ST_vec, proj_L2ball
+
+
+def fb_sqrt_lasso(A, b, alpha, max_iter=1000, verbose=0):
+    # solves using Fercoq & Bianchi
+    n_samples, n_features = A.shape
+
+    sigma = 1 / norm(A, ord=2)
+    tau = 1 / norm(A, axis=0, ord=2)
+
+    all_features = np.arange(n_features)
+    p_obj_out = []
+
+    # primal variables
+    x = np.zeros(n_features)
+
+    # dual variables
+    y = np.zeros(n_samples)
+    y_bar = np.zeros(n_samples)
+
+    for iter in range(max_iter):
+
+        for j in all_features:
+            # primal update
+            old_x_j = x[j]
+            x[j] = _prox_g_j(old_x_j - tau[j] * (A[:, j] @ (2 * y_bar - y)), tau[j],
+                             alpha, b, A)
+
+            # dual update
+            y_bar = _prox_h_star(y + sigma * (A @ x), sigma,
+                                 b, A)
+            y += (y_bar - y) / n_features
+
+            if verbose:
+                print(f"Iter {iter+1}: {_compute_obj(b, A, x, alpha):.10f}")
+
+        p_obj_out.append(_compute_obj(b, A, x, alpha))
+
+    return x, p_obj_out
+
+
+def cp_sqrt_lasso(A, b, alpha, max_iter=1000, verbose=0):
+    # using Chambolle Pock
+    n_samples, n_features = A.shape
+
+    L = norm(A, ord=2)
+    sigma = 1 / L
+    tau = 1 / L
+
+    p_obj_out = []
+
+    # primal var
+    x = np.zeros(n_features)
+    x_bar = np.zeros(n_features)
+
+    # dual var
+    y = np.zeros(n_samples)
+
+    for iter in range(max_iter):
+
+        # dual update
+        y = _prox_h_star(y + sigma * (A @ x_bar), sigma, b, A)
+
+        # primal update
+        old_x = x.copy()
+        x = _prox_g(x - tau * (A.T @ y), tau, alpha, b, A)
+        x_bar = 2 * x - old_x
+
+        if verbose:
+            print(f"Iter {iter+1}: {_compute_obj(b, A, x, alpha):.10f}")
+
+        p_obj_out.append(_compute_obj(b, A, x, alpha))
+
+    return x, p_obj_out
+
+
+# utils
+def _compute_obj(b, A, x, alpha):
+    return (norm(b - A @ x, ord=2)
+            + alpha * norm(x, ord=1))
+
+
+def _prox_g_j(x_j, step, alpha, b, A):
+    return ST(x_j, step * alpha)
+
+
+def _prox_g(x, step, alpha, b, A):
+    return ST_vec(x, step * alpha)
+
+
+def _prox_h_star(y, step, b, A):
+    return proj_L2ball(y - step * b)
--- prototype/test_protype.py
+++ prototype/test_protype.py
@@ -4,9 +4,11 @@ import numpy as np
 from numpy.linalg import norm
 
 from sklearn.linear_model import Lasso
+from skglm.experimental import SqrtLasso
 from skglm.utils import make_correlated_data
 
 from prototype.pd_lasso import cp_lasso, fb_lasso, forward_backward, cd
+from prototype.pd_sqrt_lasso import fb_sqrt_lasso, cp_sqrt_lasso
 
 
 @pytest.mark.parametrize('solver', [fb_lasso, cp_lasso, forward_backward, cd])
@@ -26,5 +28,22 @@ def test_solver(solver):
     np.testing.assert_allclose(w, lasso.coef_.flatten(), atol=1e-4)
 
 
+@pytest.mark.parametrize('solver', [fb_sqrt_lasso, cp_sqrt_lasso])
+def test_solver(solver):
+    rho = 0.1
+    n_samples, n_features = 50, 10
+    A, b, _ = make_correlated_data(n_samples, n_features,
+                                   random_state=0)
+
+    alpha_max = norm(A.T @ b, ord=np.inf) / norm(b)
+    alpha = rho * alpha_max
+
+    w, _ = solver(A, b, alpha, max_iter=1000)
+    lasso = SqrtLasso(alpha=alpha / np.sqrt(n_samples), tol=1e-9).fit(A, b)
+
+    np.testing.assert_allclose(w, lasso.coef_.flatten(), atol=1e-4)
+
+
 if __name__ == '__main__':
+    test_solver(fb_sqrt_lasso)
     pass

2ef5eb750d86f6682e25343288e911e58943240d
Badr-MOUFAD <badr.moufad@emines.um6p.ma>
2022-11-30 18:22:14 +0100

    update unittest

--- skglm/experimental/tests/test_sqrt_lasso.py
+++ skglm/experimental/tests/test_sqrt_lasso.py
@@ -9,7 +9,7 @@ from skglm.experimental.sqrt_lasso import SqrtLasso, _chambolle_pock_sqrt
 def test_alpha_max():
     n_samples, n_features = 50, 10
     X, y, _ = make_correlated_data(n_samples, n_features, random_state=0)
-    alpha_max = norm(X.T @ y, ord=np.inf) / (np.sqrt(n_samples) * norm(y))
+    alpha_max = norm(X.T @ y, ord=np.inf) / norm(y)
 
     sqrt_lasso = SqrtLasso(alpha=alpha_max).fit(X, y)
 
@@ -24,7 +24,7 @@ def test_vs_statsmodels():
     n_samples, n_features = 50, 10
     X, y, _ = make_correlated_data(n_samples, n_features, random_state=0)
 
-    alpha_max = norm(X.T @ y, ord=np.inf) / (np.sqrt(n_samples) * norm(y))
+    alpha_max = norm(X.T @ y, ord=np.inf) / norm(y)
     n_alphas = 3
     alphas = alpha_max * np.geomspace(1, 1e-2, n_alphas+1)[1:]
 
@@ -36,9 +36,10 @@ def test_vs_statsmodels():
     # fit statsmodels on path
     for i in range(n_alphas):
         alpha = alphas[i]
+        # statsmodel fits: ||y - Xw||_2 + alpha* ||w||_1 / sqrt(n_samples)
         model = linear_model.OLS(y, X)
         model = model.fit_regularized(method='sqrt_lasso', L1_wt=1.,
-                                      alpha=n_samples * alpha)
+                                      alpha=np.sqrt(n_samples) * alpha)
         coefs_statsmodels[i] = model.params
 
     np.testing.assert_almost_equal(coefs_skglm, coefs_statsmodels, decimal=4)

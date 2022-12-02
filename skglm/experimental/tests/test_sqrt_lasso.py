d0536dc1295a64d6781fa8cef61563738f608a92
Badr MOUFAD <65614794+Badr-MOUFAD@users.noreply.github.com>
2022-12-02 13:11:15 +0100

    MNT - remove normalization by ``sqrt(n_samples)`` in Square root Lasso (#130)

--- skglm/experimental/tests/test_sqrt_lasso.py
+++ skglm/experimental/tests/test_sqrt_lasso.py
@@ -9,7 +9,7 @@ from skglm.experimental.sqrt_lasso import SqrtLasso, _chambolle_pock_sqrt
 def test_alpha_max():
     n_samples, n_features = 50, 10
     X, y, _ = make_correlated_data(n_samples, n_features, random_state=0)
-    alpha_max = norm(X.T @ y, ord=np.inf) / (np.sqrt(n_samples) * norm(y))
+    alpha_max = norm(X.T @ y, ord=np.inf) / norm(y)
 
     sqrt_lasso = SqrtLasso(alpha=alpha_max).fit(X, y)
 
@@ -24,7 +24,7 @@ def test_vs_statsmodels():
     n_samples, n_features = 50, 10
     X, y, _ = make_correlated_data(n_samples, n_features, random_state=0)
 
-    alpha_max = norm(X.T @ y, ord=np.inf) / (np.sqrt(n_samples) * norm(y))
+    alpha_max = norm(X.T @ y, ord=np.inf) / norm(y)
     n_alphas = 3
     alphas = alpha_max * np.geomspace(1, 1e-2, n_alphas+1)[1:]
 
@@ -36,9 +36,10 @@ def test_vs_statsmodels():
     # fit statsmodels on path
     for i in range(n_alphas):
         alpha = alphas[i]
+        # statsmodels solves: ||y - Xw||_2 + alpha * ||w||_1 / sqrt(n_samples)
         model = linear_model.OLS(y, X)
         model = model.fit_regularized(method='sqrt_lasso', L1_wt=1.,
-                                      alpha=n_samples * alpha)
+                                      alpha=np.sqrt(n_samples) * alpha)
         coefs_statsmodels[i] = model.params
 
     np.testing.assert_almost_equal(coefs_skglm, coefs_statsmodels, decimal=4)
@@ -48,7 +49,7 @@ def test_prox_newton_cp():
     n_samples, n_features = 50, 10
     X, y, _ = make_correlated_data(n_samples, n_features, random_state=0)
 
-    alpha_max = norm(X.T @ y, ord=np.inf) / (np.sqrt(n_samples) * norm(y))
+    alpha_max = norm(X.T @ y, ord=np.inf) / norm(y)
     alpha = alpha_max / 10
     clf = SqrtLasso(alpha=alpha, tol=1e-12).fit(X, y)
     w, _, _ = _chambolle_pock_sqrt(X, y, alpha, max_iter=1000)

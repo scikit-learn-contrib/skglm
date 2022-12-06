1271288c11038105ab4e6fa21d92801426d107a1
Badr-MOUFAD <badr.moufad@emines.um6p.ma>
2022-12-06 14:27:24 +0100

    more unittest

--- skglm/experimental/tests/test_quantile_regression.py
+++ skglm/experimental/tests/test_quantile_regression.py
@@ -13,10 +13,10 @@ from sklearn.linear_model import QuantileRegressor
 @pytest.mark.parametrize('with_dual_init', [True, False])
 def test_PDCD_WS(with_dual_init, quantile=0.5):
     n_samples, n_features = 50, 10
-    X, y, _ = make_correlated_data(n_samples, n_features, random_state=0)
+    X, y, _ = make_correlated_data(n_samples, n_features, random_state=123)
 
     alpha_max = norm(X.T @ (np.sign(y)/2 + (quantile - 0.5)), ord=np.inf)
-    alpha = alpha_max
+    alpha = alpha_max / 10
 
     dual_init = np.sign(y)/2 + (quantile - 0.5) if with_dual_init else None
 
@@ -32,9 +32,23 @@ def test_PDCD_WS(with_dual_init, quantile=0.5):
     ).fit(X, y)
 
     # np.testing.assert_allclose(w, clf.coef_, atol=1e-6)
-    print(norm(w - clf.coef_))
-    print(clf.coef_)
+
+    print(
+        pin_ball_loss(y, X @ clf.coef_) + norm(clf.coef_, ord=1)
+    )
+
+    print(
+        pin_ball_loss(y, X @ w) + norm(w, ord=1)
+    )
+
+
+# taken from benchopt
+def pin_ball_loss(y_true, y_pred, quantile=0.5):
+    diff = y_true - y_pred
+    sign = (diff >= 0).astype(diff.dtype)
+    loss = quantile * sign * diff - (1 - quantile) * (1 - sign) * diff
+    return np.sum(loss)
 
 
 if __name__ == '__main__':
-    test_PDCD_WS(False)
+    test_PDCD_WS(False, quantile=0.4)

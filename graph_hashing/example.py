0587114c52c6ed232879e98a229fa64f7715a585
Badr-MOUFAD <badr.moufad@emines.um6p.ma>
2023-03-07 15:10:01 +0100

    update example & requirement in setup

--- graph_hashing/example.py
+++ graph_hashing/example.py
@@ -1,22 +1,28 @@
-import numpy
+import numpy as np
+from numpy.linalg import norm
 
-from graph_hashing.solvers import PGD, CD
-from graph_hashing.solvers.fista import FISTA
+from graph_hashing.solvers import FISTA
 from graph_hashing.solvers.utils import compute_lmbd_max
 from graph_hashing.data_src import generate_data
 
 
+#################
+###  Setups #####
+#################
 reg = 1e-1
 n_H_k, n_nodes, n_supernodes = 10, 100, 20
 
 # tensore_H_k has shape (n_H_k, n_nodes, n_supernodes)
 # tensore_S_k has shape (n_H_k, n_supernodes, n_supernodes)
-tensor_H_k, tensor_S_k, _ = generate_data(n_H_k, n_nodes, n_supernodes, n_events=100)
+tensor_H_k, tensor_S_k, S_true = generate_data(
+    n_H_k, n_nodes, n_supernodes, n_events=100)
 
 
 ########################
 ###  compute lmd max ###
 ########################
+
+# for lmbda >= lmbda max the optimal solution is 0
 lmbd_max = compute_lmbd_max(tensor_H_k, tensor_S_k)
 
 
@@ -25,8 +31,11 @@ lmbd_max = compute_lmbd_max(tensor_H_k, tensor_S_k)
 ########################
 lmbd = reg * lmbd_max
 
-S, stop_crit = CD(verbose=1, max_iter=100, tol=1e-15).solve(
+S, stop_crit = FISTA(max_iter=1000, tol=1e-15).solve(
     tensor_H_k, tensor_S_k, lmbd)
 
 # print support of solution
 print((S != 0).sum())
+
+# compare with ground truth
+print(norm(S - S_true))

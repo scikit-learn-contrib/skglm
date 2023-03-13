ff79040e6119b3c3badf4903d568f840a8979605
Badr-MOUFAD <badr.moufad@emines.um6p.ma>
2023-03-13 15:49:28 +0100

    unittest eval optimality condition

--- skglm/gpu/tests/test_utils.py
+++ skglm/gpu/tests/test_utils.py
@@ -1,5 +1,7 @@
 import numpy as np
-from skglm.gpu.utils.host_utils import compute_obj
+from skglm.gpu.utils.host_utils import compute_obj, eval_opt_crit
+
+from sklearn.linear_model import Lasso
 
 
 def test_compute_obj():
@@ -13,3 +15,21 @@ def test_compute_obj():
     p_obj = compute_obj(X, y, lmbd, w)
 
     np.testing.assert_array_equal(p_obj, 0.5 * 20 + 10. * 6)
+
+
+def test_eval_optimality():
+    rng = np.random.RandomState(1235)
+    n_samples, n_features = 10, 5
+
+    X = rng.randn(n_samples, n_features)
+    y = rng.randn(n_samples)
+    lmbd = 1.
+
+    estimator = Lasso(
+        alpha=lmbd / n_samples, fit_intercept=False, tol=1e-9
+    ).fit(X, y)
+
+    np.testing.assert_allclose(
+        eval_opt_crit(X, y, lmbd, estimator.coef_), 0.,
+        atol=1e-9
+    )

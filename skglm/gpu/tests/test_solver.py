2ad2f2ca6254b0983a20a694c3cc17d2f9b6cbe3
Badr-MOUFAD <badr.moufad@emines.um6p.ma>
2023-03-17 11:45:24 +0100

    unittest sparse data

--- skglm/gpu/tests/test_solver.py
+++ skglm/gpu/tests/test_solver.py
@@ -2,6 +2,7 @@ import pytest
 
 import numpy as np
 from numpy.linalg import norm
+from scipy import sparse
 
 from skglm.gpu.solvers import CPUSolver, CupySolver, JaxSolver, NumbaSolver
 
@@ -32,3 +33,25 @@ def test_solves(solver):
     stop_crit = eval_opt_crit(X, y, lmbd, w)
 
     np.testing.assert_allclose(stop_crit, 0., atol=1e-9)
+
+
+@pytest.mark.parametrize("solver", [CPUSolver(), CupySolver()])
+def test_solves_sparse(solver):
+    random_state = 1265
+    n_samples, n_features = 100, 30
+    reg = 1e-2
+
+    # generate dummy data
+    rng = np.random.RandomState(random_state)
+    X = sparse.rand(n_samples, n_features, density=0.1,
+                    format="csr", random_state=rng)
+    y = rng.randn(n_samples)
+
+    # set lambda
+    lmbd_max = norm(X.T @ y, ord=np.inf)
+    lmbd = reg * lmbd_max
+
+    w = solver.solve(X, y, lmbd)
+
+    stop_crit = eval_opt_crit(X, y, lmbd, w)
+    np.testing.assert_allclose(stop_crit, 0., atol=1e-7)

341bf2fb1359bba1b524dee5a3049b9886f24924
Badr-MOUFAD <badr.moufad@emines.um6p.ma>
2023-04-06 18:00:06 +0200

    implement decorator && docs

--- skglm/penalties/base.py
+++ skglm/penalties/base.py
@@ -1,3 +1,4 @@
+from numba import float64
 from abc import abstractmethod
 
 
@@ -60,3 +61,73 @@ class BasePenalty():
     @abstractmethod
     def generalized_support(self, w):
         r"""Return a mask which is True for coefficients in the generalized support."""
+
+
+def overload_with_l2(cls):
+    """Decorate a penalty class to add L2 regularization.
+
+    The resulting penalty reads
+
+    .. math::
+
+        "penalty"(w) + "l2"_"regularization" xx ||w||**2 / 2
+
+    Parameters
+    ----------
+    cls: Penalty class
+        The penalty class to be overloaded with L2 regularization.
+
+    Return
+    ------
+    cls: Penalty class
+        Penalty overloaded with L2 regularization.
+    """
+    # keep ref to original methods
+    cls_constructor = cls.__init__
+    cls_prox_1d = cls.prox_1d
+    cls_value = cls.value
+    cls_subdiff_distance = cls. subdiff_distance
+    cls_params_to_dict = cls.params_to_dict
+    cls_get_spec = cls.get_spec
+
+    # implement new methods
+    def __init__(self, *args, l2_regularization=0., **kwargs):
+        cls_constructor(self, *args, **kwargs)
+        self.l2_regularization = l2_regularization
+
+    def prox_1d(self, value, stepsize, j):
+        if self.l2_regularization == 0.:
+            return cls_prox_1d(self, value, stepsize, j)
+
+        scale = 1 + stepsize * self.l2_regularization
+        return cls_prox_1d(self, value / scale, stepsize / scale, j)
+
+    def value(self, w):
+        l2_regularization = self.l2_regularization
+        if l2_regularization == 0.:
+            return cls_value(self, w)
+
+        return cls_value(self, w) + l2_regularization * 0.5 * w ** 2
+
+    def subdiff_distance(self, w, grad, ws):
+        if self.l2_regularization == 0.:
+            return cls_subdiff_distance(self, w, grad, ws)
+
+        return cls_subdiff_distance(self, w, grad + self.l2_regularization * w[ws], ws)
+
+    def get_spec(self):
+        return (('l2_regularization', float64), *cls_get_spec(self))
+
+    def params_to_dict(self):
+        return dict(l2_regularization=self.l2_regularization,
+                    **cls_params_to_dict(self))
+
+    # override methods
+    cls.__init__ = __init__
+    cls.value = value
+    cls.prox_1d = prox_1d
+    cls.subdiff_distance = subdiff_distance
+    cls.get_spec = get_spec
+    cls.params_to_dict = params_to_dict
+
+    return cls

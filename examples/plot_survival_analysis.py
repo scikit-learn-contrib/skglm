5e0d8fa06f22f89a6cd11af329451d02177f8a97
mathurinm <mathurin.massias@gmail.com>
2023-05-31 15:27:06 +0200

    draft plot survival analysis

---
+++ examples/plot_survival_analysis.py
@@ -0,0 +1,18 @@
+# Authors: Badr Moufad
+#          Mathurin Massias
+"""
+========================================================
+Comparison of lifelines with skglm for survival analysis
+========================================================
+This example shows that skglm find the same solution as lifelines in 100 x less time.
+"""
+
+import skglm
+
+# %%
+# We first generate some synthetic data for this example
+a = 2
+# %% Let's check that our data is correct, do a plot, weibull
+
+# %%
+#

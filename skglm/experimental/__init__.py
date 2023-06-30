c7d04659f93723f8d6874a1ada21115c5cd3930b
Badr MOUFAD <65614794+Badr-MOUFAD@users.noreply.github.com>
2023-06-30 13:46:39 +0200

    MNT doc and whatsnew update pre 0.3 release (#181)

--- skglm/experimental/__init__.py
+++ skglm/experimental/__init__.py
@@ -1,9 +1,12 @@
 from .reweighted import IterativeReweightedL1
-from .sqrt_lasso import SqrtLasso
+from .sqrt_lasso import SqrtLasso, SqrtQuadratic
 from .pdcd_ws import PDCD_WS
+from .quantile_regression import Pinball
 
 __all__ = [
-    SqrtLasso,
     IterativeReweightedL1,
-    PDCD_WS
+    PDCD_WS,
+    Pinball,
+    SqrtQuadratic,
+    SqrtLasso,
 ]

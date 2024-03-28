b770d7aaa6570b04cbd6944697ee4b6c0267d65a
mathurinm <mathurin.massias@gmail.com>
2024-03-28 12:53:40 +0100

    [CI skip] isolate error

--- skglm/tests/test_estimators.py
+++ skglm/tests/test_estimators.py
@@ -601,4 +601,22 @@ def test_GroupLasso_estimator_sparse_vs_dense(fit_intercept, positive):
 
 
 if __name__ == "__main__":
-    pass
+    fit_intercept = True
+    positive = True
+    grp_indices, grp_ptr = grp_converter(groups, X.shape[1])
+    n_groups = len(grp_ptr)-1
+    weights = np.ones(n_groups)
+    alpha = _alpha_max_group_lasso(X, y, grp_indices, grp_ptr, weights) / 10.
+
+    tol = 1e-3
+    glasso = GroupLasso(groups=groups, alpha=alpha, tol=tol,
+                        weights=weights, fit_intercept=fit_intercept,
+                        positive=positive, verbose=2, max_iter=1, max_epochs=300)
+
+    X_sparse = csc_matrix(X)
+    glasso.fit(X, y)
+    coef_dense = glasso.coef_
+    # glasso.fit(X_sparse, y)
+    # coef_sparse = glasso.coef_
+
+    # np.testing.assert_allclose(coef_sparse, coef_dense)

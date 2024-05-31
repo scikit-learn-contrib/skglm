7a7e40a423fa59b48c406b7c38fea8dc6a935a65
mathurinm <mathurin.massias@gmail.com>
2024-05-31 12:41:06 +0200

    debug script for tianBao

--- debug.py
+++ debug.py
@@ -1,9 +1,14 @@
 import numpy as np
 from time import time
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from numpy.linalg import norm
+
 from skglm import GeneralizedLinearEstimator
 from skglm import datafits
 from skglm import penalties
 from skglm.solvers import ProxNewton
+
 
 # MM: make sure to seed
 rng = np.random.default_rng(0)
@@ -28,12 +33,13 @@ def simu_Y(X, n, beta, type):
             return rng.poisson(lam=probe, size=n)
 
 
-n = 200
-p = 5000
+n = 500
+p = 2000
 beta_value = np.array([5, 3, 0, 0, -2, 0] + [0] * (p - 6))
 
 start_time = time()
 X = simu_X(n, p, example='hete')
+X /= 10
 
 y = simu_Y(X, n, beta=beta_value, type='poisson')
 print('simulate data time:', time() - start_time)
@@ -45,14 +51,11 @@ print(f'{alpha_max=}')
 
 penalty.alpha = alpha_max / 100
 
-solver = ProxNewton(verbose=1, max_iter=50, fit_intercept=False)
+solver = ProxNewton(verbose=1, max_iter=50, warm_start=True, fit_intercept=False,
+                    tol=1e-4)
+
+###### single fit:
 start_time = time()
-
-
-# alpha_max = (X.T @ (1 - y) / len(y)).max()
-# print("alpha max: %1e" % alpha_max)
-# alpha = 1.1 * alpha_max  # This yields only 0 coefficients
-alpha = alpha_max / 100
 
 model = GeneralizedLinearEstimator(
     datafit=datafit, penalty=penalty, solver=solver).fit(X, y)
@@ -60,3 +63,30 @@ result = model.coef_
 print(f"fit time : {(time() - start_time):.2e} s")
 print( np.where(result !=0)[0] )
 print( result[np.where(result !=0)[0]] )
+
+
+###### Grid Search
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+alpha_grid = np.geomspace(alpha_max, alpha_max / 10000, num=200)
+
+rmses_train = []
+rmses_test = []
+loss_train = []
+loss_test = []
+for alpha in alpha_grid:
+    penalty.alpha = alpha
+    model.fit(X_train, y_train)
+    rmses_test.append(norm(y_test - model.predict(X_test)) / np.sqrt(X.shape[0]))
+    rmses_train.append(norm(y_train - model.predict(X_train)) / np.sqrt(X.shape[0]))
+    loss_train.append(datafit.value(y_train, model.coef_, X_train @ model.coef_))
+    loss_test.append(datafit.value(y_test, model.coef_, X_test @ model.coef_))
+
+plt.close()
+plt.loglog(alpha_grid, rmses_test, label='test RMSE', color="tab10:orange")
+plt.loglog(alpha_grid, rmses_train, label='train RMSE', color="tab10:orange", linestyle='--')
+plt.loglog(alpha_grid, loss_test, label='test datafitting loss', color="tab10:blue")
+plt.loglog(alpha_grid, loss_train, label='train datafitting loss',
+           color="tab10:blue", linestyle='--')
+plt.legend()
+plt.xlabel("regularization strength alpha")
+plt.show(block=False)

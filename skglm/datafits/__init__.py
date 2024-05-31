f764f10560a0a1bf8623fd8af9c4840ad5b1c773
sujay_pandit <sujay9629@gmail.com>
2024-05-31 13:42:18 -0400

    Datafit support for sample weights; tested with plot_sparse_recovery.py

--- skglm/datafits/__init__.py
+++ skglm/datafits/__init__.py
@@ -1,5 +1,5 @@
 from .base import BaseDatafit, BaseMultitaskDatafit
-from .single_task import Quadratic, QuadraticSVC, Logistic, Huber, Poisson, Gamma, Cox
+from .single_task import Quadratic, QuadraticSVC, Logistic, Huber, Poisson, Gamma, Cox, WeightedQuadratic
 from .multi_task import QuadraticMultiTask
 from .group import QuadraticGroup, LogisticGroup
 
@@ -8,5 +8,5 @@ __all__ = [
     BaseDatafit, BaseMultitaskDatafit,
     Quadratic, QuadraticSVC, Logistic, Huber, Poisson, Gamma, Cox,
     QuadraticMultiTask,
-    QuadraticGroup, LogisticGroup
+    QuadraticGroup, LogisticGroup, WeightedQuadratic
 ]

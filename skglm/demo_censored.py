a35453556c0c6cc6c6bd7f8dd45e11b73a2ab768
QB3 <quentin.bertrand@inria.fr>
2024-06-27 06:40:19 -0400

    [ci skip] loaded X_csc magenpy data

--- skglm/demo_censored.py
+++ skglm/demo_censored.py
@@ -22,3 +22,15 @@ df2 = compiled_clone(df2)
 
 w2 = solver.solve(X, np.zeros(X.shape[0]), df2, pen)[0]
 np.testing.assert_allclose(w2, w)
+
+
+###########################################
+# Load the design matrix
+bed_file = "../magenpy/magenpy/data/1000G_eur_chr22.bed"
+import os.path
+import scipy
+from pandas_plink import read_plink1_bin
+
+assert os.path.isfile(bed_file)
+X_dask = read_plink1_bin(bed_file, ref="a0", verbose=False)
+X_csc = scipy.sparse.csc_matrix(X_dask)

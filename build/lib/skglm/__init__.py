dabff746ebff151a9cf52d44f4cf06617dd0e115
floriankozikowski <florian.kozikowski@polytechnique.edu>
2025-04-15 17:47:44 +0200

    cleanup

--- build/lib/skglm/__init__.py
+++
@@ -1,6 +0,0 @@
-__version__ = '0.4dev'
-
-from skglm.estimators import (  # noqa F401
-    Lasso, WeightedLasso, ElasticNet, MCPRegression, MultiTaskLasso, LinearSVC,
-    SparseLogisticRegression, GeneralizedLinearEstimator, CoxEstimator, GroupLasso,
-)

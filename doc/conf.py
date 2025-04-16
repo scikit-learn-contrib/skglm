17df265aa64f2b1f58041519f4371e99aaf753fd
floriankozikowski <florian.kozikowski@polytechnique.edu>
2025-04-16 15:32:18 +0200

    make navbar responsive

--- doc/conf.py
+++ doc/conf.py
@@ -78,9 +78,6 @@ myst_enable_extensions = [
 # generate autosummary even if no references
 autosummary_generate = True
 
-# generate autosummary even if no references
-autosummary_generate = True
-
 templates_path = ['_templates']
 
 html_css_files = ["style.css"]
@@ -129,28 +126,25 @@ html_theme_options = {
         "color-brand-content": primary_dark,
         "color-brand-secondary": primary,
     },
-    "navbar_align": "right",
+    "navbar_align": "left",
     "navbar_start": ["navbar-logo"],
     "navbar_center": ["navbar-nav"],
-    "navbar_end": ["navbar-icon-links"],
     "icon_links": [
         {
             "name": "GitHub",
             "url": "https://github.com/scikit-learn-contrib/skglm",
-            "icon": "fa-brands fa-github",
+            "icon": "fab fa-github",
+            "type": "fontawesome",
         },
     ],
+    "navbar_end": ["navbar-icon-links"],
+    "navigation_with_keys": True,
     "search_bar_text": "Search skglm docs...",
     "footer_icons": [
         {
             "name": "GitHub",
             "url": "https://github.com/scikit-learn-contrib/skglm",
-            "html": """
-                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
-                </svg>
-            """,
-            "class": "",
+            "icon": "fa-brands fa-github",
         },
     ],
 }
@@ -389,5 +383,3 @@ sphinx_gallery_conf = {
         'skglm': None,
     }
 }
-
-numpydoc_show_class_members = False

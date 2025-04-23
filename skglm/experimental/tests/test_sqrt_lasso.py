bd8134508249173c46194a9a7cef03a59de5757c
floriankozikowski <florian.kozikowski@polytechnique.edu>
2025-04-23 14:00:48 +0200

    add unit test for sqrt lasso with intercept

--- skglm/experimental/tests/test_sqrt_lasso.py
+++ skglm/experimental/tests/test_sqrt_lasso.py
@@ -7,6 +7,7 @@ from skglm.utils.data import make_correlated_data
 from skglm.experimental.sqrt_lasso import (SqrtLasso, SqrtQuadratic,
                                            _chambolle_pock_sqrt)
 from skglm.experimental.pdcd_ws import PDCD_WS
+from skglm import Lasso
 
 
 def test_alpha_max():
@@ -78,6 +79,23 @@ def test_PDCD_WS(with_dual_init):
     w = PDCD_WS(dual_init=dual_init).solve(X, y, datafit, penalty)[0]
     clf = SqrtLasso(alpha=alpha, fit_intercept=False, tol=1e-12).fit(X, y)
     np.testing.assert_allclose(clf.coef_, w, atol=1e-6)
+
+
+def test_lasso_vs_sqrt_lasso_with_intercept():
+
+    n_samples, n_features = 50, 10
+    X, y, _ = make_correlated_data(n_samples, n_features, random_state=0)
+
+    alpha_max = norm(X.T @ y, ord=np.inf) / norm(y)
+    alpha = alpha_max / 10
+
+    lasso = Lasso(alpha=alpha, fit_intercept=True, tol=1e-8).fit(X, y)
+    w_lasso = lasso.coef_
+
+    scal = n_samples / norm(y - lasso.predict(X))
+    sqrt = SqrtLasso(alpha=alpha * scal, fit_intercept=True, tol=1e-8).fit(X, y)
+
+    np.testing.assert_allclose(w_lasso, sqrt.coef_, rtol=1e-6)
 
 
 if __name__ == '__main__':

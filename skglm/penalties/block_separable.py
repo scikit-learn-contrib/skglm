e3dbd534cb62f45fe312c34e40953ee5f3060d29
Johan Larsson <johan@jolars.co>
2026-05-13 10:39:28 +0200

    MNT reduce line length to fix flake8 lint

--- skglm/penalties/block_separable.py
+++ skglm/penalties/block_separable.py
@@ -457,7 +457,9 @@ class WeightedL1GroupL2(BasePenalty):
         """Compute the proximal operator of group ``g``."""
         grp_ptr, grp_indices = self.grp_ptr, self.grp_indices
         grp_g_indices = grp_indices[grp_ptr[g]: grp_ptr[g+1]]
-        res = ST_vec(value, self.alpha * stepsize * self.weights_features[grp_g_indices])
+        res = ST_vec(
+            value, self.alpha * stepsize * self.weights_features[grp_g_indices]
+        )
         return BST(res, self.alpha * stepsize * self.weights_groups[g])
 
     def is_penalized(self, n_groups):
